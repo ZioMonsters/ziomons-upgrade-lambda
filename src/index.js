85fc9e9aaa6b4f27b46dd30bf69729a3739b4ca2
Emanuele Caruso <ema.caruso2@gmail.com>
2018-09-08 17:10:08 +0000

    toTest

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-const AWS = require("aws-sdk")
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient({region:'eu-west-3'});
 
 exports.handler = (event, context, callback) => {
-  callback(null, event)
+  const { eventId, _id, _atkMod, _defMod, _spdMod } = JSON.parse(event.Records[0].body);
+
+  Promise.all([
+      documentClient.upgrade({
+          TableName: `cryptomon-monsters-${process.env.NODE_ENV}`,
+          Key: {monsterId: _id},
+          AttributeUpdates: {
+              'attack': {
+                  Action: 'ADD',
+                  Value: _atkMod
+              },
+              'defense': {
+                  Action: 'ADD',
+                  Value: _defMod
+              },
+              'speed': {
+                  Action: 'ADD',
+                  Value: _spdMod
+              }
+          }
+      }).promise(),
+      documentClient.put({
+          TableName: `cryptomon-events-${process.env.NODE_ENV}`,
+          Item: {
+              transactionId: eventId,
+              type: 'upgrade',
+              processed: true
+          }
+      }).promise()
+  ])
+  .then(() => callback(null, event))
+  .catch(console.error);
 }
